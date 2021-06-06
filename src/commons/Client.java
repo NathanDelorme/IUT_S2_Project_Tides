7e8b9592474ad562e636a02cffe2ebfc493be6c9
Nathan Delorme <nathandelorme2001@gmail.com>
2021-06-06 20:24:41 +0200

    Initial project commit Client

---
+++ src/commons/Client.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package commons;
+
+/**
+ * @author Nathan Delorme
+ *
+ */
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
